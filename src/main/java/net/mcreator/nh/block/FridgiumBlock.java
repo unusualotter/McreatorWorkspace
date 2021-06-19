a1fa13b0b73f141c105044180a69c11a4184e379
unusualotter <korbinsilas@gmail.com>
2021-06-19 15:27:50 -0400

    Added fridgium

---
+++ src/main/java/net/mcreator/nh/block/FridgiumBlock.java
@@ -0,0 +1,115 @@
+
+package net.mcreator.nh.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fluids.ForgeFlowingFluid;
+import net.minecraftforge.fluids.FluidAttributes;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.gen.placement.Placement;
+import net.minecraft.world.gen.placement.ChanceConfig;
+import net.minecraft.world.gen.feature.LakesFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.BlockStateFeatureConfig;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.World;
+import net.minecraft.world.ISeedReader;
+import net.minecraft.util.registry.WorldGenRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BucketItem;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.FlowingFluid;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.FlowingFluidBlock;
+import net.minecraft.block.Block;
+
+import net.mcreator.nh.NhModElements;
+
+import java.util.Random;
+
+@NhModElements.ModElement.Tag
+public class FridgiumBlock extends NhModElements.ModElement {
+	@ObjectHolder("nh:fridgium")
+	public static final FlowingFluidBlock block = null;
+	@ObjectHolder("nh:fridgium_bucket")
+	public static final Item bucket = null;
+	public static FlowingFluid flowing = null;
+	public static FlowingFluid still = null;
+	private ForgeFlowingFluid.Properties fluidproperties = null;
+	public FridgiumBlock(NhModElements instance) {
+		super(instance, 36);
+		FMLJavaModLoadingContext.get().getModEventBus().register(new FluidRegisterHandler());
+		MinecraftForge.EVENT_BUS.register(this);
+		FMLJavaModLoadingContext.get().getModEventBus().register(new FeatureRegisterHandler());
+	}
+	private static class FluidRegisterHandler {
+		@SubscribeEvent
+		public void registerFluids(RegistryEvent.Register<Fluid> event) {
+			event.getRegistry().register(still);
+			event.getRegistry().register(flowing);
+		}
+	}
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientLoad(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(still, RenderType.getTranslucent());
+		RenderTypeLookup.setRenderLayer(flowing, RenderType.getTranslucent());
+	}
+
+	@Override
+	public void initElements() {
+		fluidproperties = new ForgeFlowingFluid.Properties(() -> still, () -> flowing,
+				FluidAttributes.builder(new ResourceLocation("nh:blocks/fridgium"), new ResourceLocation("nh:blocks/fridgium")).luminosity(1)
+						.density(-800).viscosity(500).gaseous()).bucket(() -> bucket).block(() -> block);
+		still = (FlowingFluid) new ForgeFlowingFluid.Source(fluidproperties).setRegistryName("fridgium");
+		flowing = (FlowingFluid) new ForgeFlowingFluid.Flowing(fluidproperties).setRegistryName("fridgium_flowing");
+		elements.blocks.add(() -> new FlowingFluidBlock(still, Block.Properties.create(Material.WATER)) {
+		}.setRegistryName("fridgium"));
+		elements.items.add(() -> new BucketItem(still, new Item.Properties().containerItem(Items.BUCKET).maxStackSize(1).group(ItemGroup.MISC))
+				.setRegistryName("fridgium_bucket"));
+	}
+	private static Feature<BlockStateFeatureConfig> feature = null;
+	private static ConfiguredFeature<?, ?> configuredFeature = null;
+	private static class FeatureRegisterHandler {
+		@SubscribeEvent
+		public void registerFeature(RegistryEvent.Register<Feature<?>> event) {
+			feature = new LakesFeature(BlockStateFeatureConfig.field_236455_a_) {
+				@Override
+				public boolean generate(ISeedReader world, ChunkGenerator generator, Random rand, BlockPos pos, BlockStateFeatureConfig config) {
+					RegistryKey<World> dimensionType = world.getWorld().getDimensionKey();
+					boolean dimensionCriteria = false;
+					if (dimensionType == World.THE_NETHER)
+						dimensionCriteria = true;
+					if (!dimensionCriteria)
+						return false;
+					return super.generate(world, generator, rand, pos, config);
+				}
+			};
+			configuredFeature = feature.withConfiguration(new BlockStateFeatureConfig(block.getDefaultState()))
+					.withPlacement(Placement.WATER_LAKE.configure(new ChanceConfig(5)));
+			event.getRegistry().register(feature.setRegistryName("fridgium_lakes"));
+			Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation("nh:fridgium_lakes"), configuredFeature);
+		}
+	}
+	@SubscribeEvent
+	public void addFeatureToBiomes(BiomeLoadingEvent event) {
+		event.getGeneration().getFeatures(GenerationStage.Decoration.LOCAL_MODIFICATIONS).add(() -> configuredFeature);
+	}
+}
